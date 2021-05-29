e216400e94023602875bf8ce959692042bc70423
Araf020 <Araf020@users.noreply.github.com>
2021-05-29 21:57:52 +0600

    structural design pattern

--- src/com/arafat/structural/addapter/AdapterDriver.java
+++ src/com/arafat/structural/addapter/AdapterDriver.java
@@ -3,17 +3,30 @@ package com.arafat.structural.addapter;
 import com.arafat.structural.addapter.connect.Calculate;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class AdapterDriver {
     public static void main(String[] args) throws IOException {
         Calculate calculate = new Calculate();
         FileReader fr =
-                new FileReader("E:\\Structural Design Pattern\\test.txt");
+                new FileReader("test.txt");
 
-        File file = new File("E:\\Structural Design Pattern\\test.txt");
+        File file = new File("test.txt");
 
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        System.out.println("Choose a file type:\n   1. Integer\n   2.Character\n");
 
-        calculate.calculateSum(fr,"integer");
+        Scanner scanner = new Scanner(System.in);
+
+        String filetype = scanner.nextLine();
+        if (filetype.equals("1"))
+            calculate.calculateSum(fr,"integer");
+        else if (filetype.equals("2")){
+            calculate.calculateSum(fr,"character");
+
+        }
+        else {
+            System.out.println("Invalid input type");
+        }
     }
 }
